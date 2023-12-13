7f6ff642aac8b555c8afe4623232bc72893d0812
Rajwinder Singh <irajwinder@users.noreply.github.com>
2023-12-13 07:52:09 -0500

    Initial Commit

---
+++ GraphQL/GraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQLApp.swift
+//  GraphQL
+//
+//  Created by Rajwinder Singh on 12/13/23.
+//
+
+import SwiftUI
+
+@main
+struct GraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
